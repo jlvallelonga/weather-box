4980d24daedadc1233352d888d15f90e05c38ffa
Josh Ashby <joshuaashby@joshashby.com>
2014-07-17 10:18:41 -0600

    comments and indents

--- internetTime.py
+++ internetTime.py
@@ -2,14 +2,21 @@ import requests
 import arrow
 
 def getTime():
-  res = requests.get('http://timeapi.org/utc/now")
-  if res.status_code == 200:
-    time = arrow.get(res.content)
-    return time.to("US/Mountain").format("hh:mm a")
-  else
-    return ""
+    """
+    Returns a string in the format of Hour(12):Minutes am/pm
+    """
+    res = requests.get('http://timeapi.org/utc/now")
+    if res.status_code == 200:
+        time = arrow.get(res.content)
+        return time.to("US/Mountain").format("hh:mm a")
+    else
+        return ""
 
 class InternetTime:
     @staticmethod
     def getTime():
+        """
+        Return getTime() from above, this exists as just a
+        backwards compatability thing for now.
+        """
         return getTime()
